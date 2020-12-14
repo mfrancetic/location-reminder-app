724488b6cc311cea1f0839454b9c01482bd1decb
Maja Francetic <francetic.maja@gmail.com>
2020-12-14 20:36:35 +0100

    Implement testing the back and up buttons in the AppNavigationTest

--- starter/app/src/androidTest/java/com/udacity/project4/AppNavigationTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/AppNavigationTest.kt
@@ -1,4 +1,169 @@
-package com.udacity.project4.locationreminders
+package com.udacity.project4
 
-class AppNavigationTest {
+import android.app.Application
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.udacity.project4.authentication.AuthenticationViewModel
+import com.udacity.project4.locationreminders.RemindersActivity
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
+import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.locationreminders.savereminder.selectreminderlocation.SelectLocationViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
+import com.udacity.project4.utils.EspressoIdlingResource
+import it.xabaras.android.espresso.recyclerviewchildactions.RecyclerViewChildActions
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+import org.koin.test.get
+
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AppNavigationTest : AutoCloseKoinTest() {
+
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+
+    private lateinit var repository: ReminderDataSource
+    private lateinit var appContext: Application
+
+    @Before
+    fun initDependencyInjectionAndRegisterIdlingResources() = runBlocking {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+
+        stopKoin()//stop the original app koin
+        appContext = ApplicationProvider.getApplicationContext()
+        val myModule = module {
+            viewModel {
+                RemindersListViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            viewModel {
+                AuthenticationViewModel(
+                    appContext,
+                )
+            }
+            viewModel {
+                SelectLocationViewModel(
+                    appContext,
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { RemindersLocalRepository(get()) }
+            single { LocalDB.createRemindersDao(appContext) }
+        }
+        //declare a new koin module
+        startKoin {
+            modules(listOf(myModule))
+        }
+        //Get our real repository
+        repository = get()
+
+        //clear the data to start fresh
+        runBlocking {
+            repository.deleteAllReminders()
+        }
+        val reminder1 = ReminderDTO(
+            "title1", "description1", "location1",
+            11.111, 11.112
+        )
+        repository.saveReminder(reminder1)
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
+    }
+
+    @Test
+    fun reminderDescriptionScreen_backButton(): Unit = runBlocking {
+        val reminder1 = ReminderDTO(
+            "title1", "description1", "location1",
+            11.111, 11.112
+        )
+        repository.saveReminder(reminder1)
+
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.reminderssRecyclerView)).perform(
+            RecyclerViewChildActions.Companion.actionOnChild(
+                click(), R.id.reminderCardView
+            )
+        )
+
+        Espresso.pressBack()
+
+        onView(
+            withId(R.id.reminderssRecyclerView)
+        )
+            .check(matches(hasDescendant(withText("title1"))))
+
+        activityScenario.close()
+    }
+
+    @Test
+    fun selectReminderScreen_doubleUpButton() = runBlocking {
+        val reminder1 = ReminderDTO(
+            "title1", "description1", "location1",
+            11.111, 11.112
+        )
+        repository.saveReminder(reminder1)
+
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).check(matches(isDisplayed()))
+
+        val upButton = onView(
+            Matchers.allOf(
+                withContentDescription("Navigate up"),
+                isDisplayed()
+            )
+        )
+        upButton.perform(click())
+
+        onView(withId(R.id.saveReminder))
+            .check(matches(isDisplayed()))
+
+        upButton.perform(click())
+
+        onView(withId(R.id.reminderssRecyclerView))
+            .check(matches(isDisplayed()))
+
+        activityScenario.close()
+    }
 }
