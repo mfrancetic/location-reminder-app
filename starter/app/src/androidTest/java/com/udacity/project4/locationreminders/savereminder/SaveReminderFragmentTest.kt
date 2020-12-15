162c8f5014b173e791a0b075dfb4b35c133f9984
Maja Francetic <francetic.maja@gmail.com>
2020-12-15 11:20:18 +0100

    Implement testing for SaveReminderFragmentTest

---
+++ starter/app/src/androidTest/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragmentTest.kt
@@ -0,0 +1,104 @@
+package com.udacity.project4.locationreminders.savereminder
+
+import android.app.Application
+import android.os.Bundle
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.MediumTest
+import com.udacity.project4.MainAndroidTestCoroutineRule
+import com.udacity.project4.R
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.local.LocalDB
+import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
+import com.udacity.project4.locationreminders.savereminder.selectreminderlocation.SelectLocationViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.context.startKoin
+import org.koin.core.context.stopKoin
+import org.koin.dsl.module
+import org.koin.test.AutoCloseKoinTest
+
+@RunWith(AndroidJUnit4::class)
+@ExperimentalCoroutinesApi
+//UI Testing
+@MediumTest
+class SaveReminderFragmentTest : AutoCloseKoinTest() {
+
+    @get: Rule
+    val mainCoroutineRule = MainAndroidTestCoroutineRule()
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var appContext: Application
+
+    @Before
+    fun init() {
+        stopKoin()//stop the original app koin
+        appContext = ApplicationProvider.getApplicationContext()
+        val myModule = module {
+            viewModel {
+                SelectLocationViewModel(
+                    appContext,
+                )
+            }
+            single {
+                SaveReminderViewModel(
+                    appContext,
+                    get() as ReminderDataSource
+                )
+            }
+            single { RemindersLocalRepository(get()) as ReminderDataSource }
+            single { RemindersLocalRepository(get()) }
+            single { LocalDB.createRemindersDao(appContext) }
+        }
+        startKoin {
+            modules(listOf(myModule))
+        }
+    }
+
+    @After
+    fun cleanupDb() {
+        stopKoin()
+    }
+
+    @Test
+    fun saveReminderScreen_isDisplayed() {
+        launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
+
+        Espresso.onView(ViewMatchers.withId(R.id.reminderTitle))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.reminderDescription))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.selectLocation))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+        Espresso.onView(ViewMatchers.withId(R.id.saveReminder))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun reminderLocationMissing_displaysSnackbar() {
+        launchFragmentInContainer<SaveReminderFragment>(Bundle(), R.style.AppTheme)
+
+        Espresso.onView(ViewMatchers.withId(R.id.reminderTitle))
+            .perform(ViewActions.replaceText("title2"))
+        Espresso.onView(ViewMatchers.withId(R.id.reminderDescription))
+            .perform(ViewActions.replaceText("description2"))
+        Espresso.onView(ViewMatchers.withId(R.id.saveReminder)).perform(ViewActions.click())
+
+        val snackbarSelectLocationMessage = appContext.getString(R.string.select_location)
+        Espresso.onView(ViewMatchers.withText(snackbarSelectLocationMessage))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+}
